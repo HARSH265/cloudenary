d306b97269dc79f1cd4aa2c33b7827b8de7e6f75
harshit jadoun <harshitjadaundipl@gmail.com>
2024-07-30 16:06:54 +0530

    gallary using cloudnery

---
+++ frontend/src/App.jsx
@@ -0,0 +1,19 @@
+import './App.css';
+import ImageDetails from './ImageDetails';
+import ImageGallery from './ImageGallery';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+
+function App() {
+  return (
+    <div className='App'>
+      <BrowserRouter>
+        <Routes>
+          <Route path='/' element={<ImageGallery />} />
+          <Route path='/:id' element={<ImageDetails />} />
+        </Routes>
+      </BrowserRouter>
+    </div>
+  );
+}
+
+export default App;
